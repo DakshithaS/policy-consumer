ae9c03411231004195ce4cbb4a496eaffcae4d83
dakshithas <dakshitha.sur@gmail.com>
2025-12-07 14:13:05 +0530

    Update module paths to include full repo path

--- main.go
+++ main.go
@@ -1,19 +1,18 @@
-package consumerproject
 package main
 
 import (
+	"fmt"
+	"time"
 
+	ratelimiter "github.com/DakshithaS/test-policy-hub/rate-limiter/v1.0.0"
+)
 
-
-
-
-
-
-
-
-
-
-
-
-
-}	}		fmt.Println("Request denied")	} else {		fmt.Println("Request allowed")	if rl.Allow() {	fmt.Println("Rate limiter created")	rl := ratelimiter.NewRateLimiter(10, time.Minute)func main() {)	ratelimiter "github.com/DakshithaS/test-policy-hub/rate-limiter/v1.0.0"	"time"	"fmt"
+func main() {
+	rl := ratelimiter.NewRateLimiter(10, time.Minute)
+	fmt.Println("Rate limiter created")
+	if rl.Allow() {
+		fmt.Println("Request allowed")
+	} else {
+		fmt.Println("Request denied")
+	}
+}
